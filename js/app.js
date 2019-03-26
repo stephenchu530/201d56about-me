95cfdce530ab59871b37710b84d32e434dba8f42
STEPHEN CHU <stephen.chu530@gmail.com>
2019-03-26 16:23:37 -0700

    style: changed variable name

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 
 // All answers are yes!!!
 let questionBank = ['Do you think I like the color black?', 'Could I possibly like to eat sushi?', 'Was I in the Air Force?', 'Do I like to watch cooking shows?', 'Is sleeping my favorite activity?'];
-let responseBank = ['Wrong!', 'Right!', 'Not an acceptable response!'];
+
+let userFeedbackBank = ['Wrong!', 'Right!', 'Not an acceptable response!'];
 let possibleAns = ['y', 'n', 'yes', 'no'];
 let cue = ' (y/n/yes/no):';
 let answer = '';
@@ -16,12 +17,12 @@ for (let i = 0; i < questionBank.length; i++) {
   idx = possibleAns.indexOf(answer);
   if (idx + 1) {
     if (idx % 2) {
-      alert(responseBank[0]);
+      alert(userFeedbackBank[0]);
     } else {
-      alert(responseBank[1]);
+      alert(userFeedbackBank[1]);
     }
   } else {
-    alert(responseBank[2]);
+    alert(userFeedbackBank[2]);
     console.log('Not acceptable. Repeating the same question.');
     i--;
   }
