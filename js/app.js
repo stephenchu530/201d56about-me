a06ebe9745ed24696eb810ae6b7007472fbf7d24
STEPHEN CHU <stephen.chu530@gmail.com>
2019-03-27 14:53:55 -0700

    feat: added null check for user name input

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 'use strict';
 
-// All answers are yes!!!
+// Get User Name and give intro to guessing game
+let userName = '';
+do {
+  userName = prompt('What is your name?');
+  console.log('User entered: ' + userName + ' for name.');
+  if (userName === null) {
+    alert('Please enter a name!');
+  }
+} while (userName === null);
+alert('Hello ' + userName + '! Let\'s play a guessing game about me. We\'ll start with 5 yes/no questions about me!');
+
+
+// 5 yes no questions; All answers are yes btw
 let questionBank = [
   'Do you think I like the color black?',
   'Could I possibly like to eat sushi?',
@@ -8,7 +20,6 @@ let questionBank = [
   'Do I like to watch cooking shows?',
   'Is sleeping my favorite activity?'
 ];
-
 let userFeedbackBank = [
   'Wrong!',
   'Right!',
@@ -21,18 +32,12 @@ let possibleAns = [
   'no'
 ];
 let cue = ' (y/n/yes/no):';
-let answer = '';
-let idx = 0;
-let userName = prompt('What is your name?');
-
-console.log('User entered: ' + userName + ' for name.');
-alert('Hello ' + userName + '! Let\'s play a guessing game about me.');
 
 for (let i = 0; i < questionBank.length; i++) {
-  answer = prompt(questionBank[i] + cue).toLowerCase();
+  let answer = prompt(questionBank[i] + cue);
   console.log('Question: ' + questionBank[i]);
   console.log('Response in all lower case: ' + answer);
-  idx = possibleAns.indexOf(answer);
+  let idx = possibleAns.indexOf(answer.toLowerCase());
   if (idx + 1) {
     if (idx % 2) {
       alert(userFeedbackBank[0]);
@@ -46,9 +51,11 @@ for (let i = 0; i < questionBank.length; i++) {
   }
 }
 
+
+// Display answers to 5 questions and correct answers on the page
 let listItemsQuestions = '';
 
 for (let i = 0; i < questionBank.length; i++) {
   listItemsQuestions += '<li>' + questionBank[i] + ' YES!</li>\n';
 }
-document.getElementById('questions').innerHTML = listItemsQuestions;
+document.getElementById('fivequestionlist').innerHTML = listItemsQuestions;
