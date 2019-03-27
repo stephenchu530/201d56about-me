1556a5eee4cf9dfcd846ce70a8f761e04bff599e
STEPHEN CHU <stephen.chu530@gmail.com>
2019-03-27 15:43:18 -0700

    fix: added check for cancel out of prompt

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 'use strict';
 
 // Get User Name and give intro to guessing game
-let userName = '';
+let userName = null;
 do {
   userName = prompt('What is your name?');
   console.log('User entered: ' + userName + ' for name.');
-  if (userName === null) {
-    alert('Please enter a name!');
-  }
+  if (userName) {break;}
+  alert('Please enter a name!');
 } while (userName === null);
 alert('Hello ' + userName + '! Let\'s play a guessing game about me. We\'ll start with 5 yes/no questions about me!');
 
+
+let correctAnswers = 0;
 
 // 5 yes no questions; All answers are yes btw
 let questionBank = [
@@ -36,13 +37,17 @@ let cue = ' (y/n/yes/no):';
 for (let i = 0; i < questionBank.length; i++) {
   let answer = prompt(questionBank[i] + cue);
   console.log('Question: ' + questionBank[i]);
-  console.log('Response in all lower case: ' + answer);
-  let idx = possibleAns.indexOf(answer.toLowerCase());
-  if (idx + 1) {
+  console.log('User response: ' + answer);
+  let idx = -1;
+  if (answer) {
+    idx = possibleAns.indexOf(answer.toLowerCase());
+  }
+  if (answer && idx + 1) {
     if (idx % 2) {
       alert(userFeedbackBank[0]);
     } else {
       alert(userFeedbackBank[1]);
+      correctAnswers++;
     }
   } else {
     alert(userFeedbackBank[2]);
