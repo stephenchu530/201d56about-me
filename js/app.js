be9686ea58127a3fef008e1cd1746143724617ae
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-03-28 13:31:06 -0700

    updated js with getUserName function

--- js/app.js
+++ js/app.js
@@ -1,16 +1,111 @@
+// 'use strict';
+
+// // Get User Name and give intro to guessing game
+// let userName = null;
+// do {
+//   userName = prompt('What is your name?');
+//   console.log('User entered: ' + userName + ' for name.');
+//   if (userName) {break;}
+//   alert('Please enter a name!');
+// } while (userName === null);
+// alert('Hello ' + userName + '! Let\'s play a guessing game about me. We\'ll start with 5 yes/no questions about me!');
+
+
+// // Keeps track of all correct responses
+// let correctAnswers = 0;
+
+
+// // 5 yes no questionaire; FYI: all answers are yes btw
+// console.log('*** Starting 5 yes/no questions game');
+// let questionBank = [
+//   'Do you think I like the color black?',
+//   'Could I possibly like to eat sushi?',
+//   'Was I in the Air Force?',
+//   'Do I like to watch cooking shows?',
+//   'Is sleeping my favorite activity?'
+// ];
+// let possibleAns = [
+//   'y',
+//   'n',
+//   'yes',
+//   'no'
+// ];
+
+// for (let i = 0; i < questionBank.length; i++) {
+//   let answer = prompt(questionBank[i] + ' (y/n/yes/no):');
+//   console.log('Question: ' + questionBank[i]);
+//   console.log('User response: ' + answer);
+//   let idx = -1;
+//   if (answer) { idx = possibleAns.indexOf(answer.toLowerCase()); }
+//   if (answer && (idx + 1)) {
+//     if (idx % 2) {
+//       alert('Wrong!');
+//     } else {
+//       alert('Right!');
+//       correctAnswers++;
+//     }
+//   } else {
+//     alert('Not an acceptable response!');
+//     console.log('Not acceptable. Repeating the same question.');
+//     i--;
+//   }
+// }
+
+// // Display answers to 5 questions and correct answers on the page
+// let listItemsQuestions = '';
+
+// for (let i = 0; i < questionBank.length; i++) {
+//   listItemsQuestions += '<li>' + questionBank[i] + ' YES!</li>\n';
+// }
+// document.getElementById('fivequestionlist').innerHTML = listItemsQuestions;
+// alert('You got ' + correctAnswers + ' out of 5 correct so far!');
+
+
+// //Favorite number game
+// console.log('*** Starting guess number game');
+// let maxNumber = 15;
+// let maxGuesses = 4;
+// let guessWord = ' guesses';
+// let response = null;
+// let randomNumber = Math.floor(Math.random() * Math.floor(maxNumber + 1));
+// alert('Let\'s play guess my number game!');
+
+// while (parseInt(response) !== randomNumber && (maxGuesses > 0)) {
+//   response = prompt('Guess my number between 0 and ' + maxNumber + ' (' + maxGuesses + guessWord + ' remaining)');
+//   console.log('User guessed: ' + response);
+//   if (response && !isNaN(response)) {
+//     if (parseInt(response) > randomNumber) {
+//       alert('Mmmm... the number is lower');
+//     }
+//     if (parseInt(response) < randomNumber) {
+//       alert('Actually, the number is higher');
+//     }
+//     maxGuesses--;
+//     if (maxGuesses === 1) {
+//       guessWord = ' guess';
+//     }
+//   } else {
+//     alert('Please enter a number!');
+//   }
+// }
+
 'use strict';
 
 // Get User Name and give intro to guessing game
-let userName = null;
-do {
-  userName = prompt('What is your name?');
-  console.log('User entered: ' + userName + ' for name.');
-  if (userName) {break;}
-  alert('Please enter a name!');
-} while (userName === null);
+let userName = getUserName();
+
 alert('Hello ' + userName + '! Let\'s play a guessing game about me. We\'ll start with 5 yes/no questions about me!');
 
-
+function getUserName(){
+  let userName = null;
+  do {
+    userName = prompt('What is your name?');
+    console.log('User entered: ' + userName + ' for name.');
+    if (userName) {break;}
+    alert('Please enter a name!');
+  } while (userName === null || userName === '');
+  return userName;
+}
 // Keeps track of all correct responses
 let correctAnswers = 0;
 
@@ -88,6 +183,7 @@ while (parseInt(response) !== randomNumber && (maxGuesses > 0)) {
     alert('Please enter a number!');
   }
 }
+
 
 // Results for the guess the number game
 if (parseInt(response) === randomNumber) {
