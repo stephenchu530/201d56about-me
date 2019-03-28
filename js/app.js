ddeb3d489932adf2c1663df9e62084600480de0c
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-03-28 14:16:21 -0700

    updaatedgame 3 functions

--- js/app.js
+++ js/app.js
@@ -1,108 +1,24 @@
-// 'use strict';
-
-// // Get User Name and give intro to guessing game
-// let userName = null;
-// do {
-//   userName = prompt('What is your name?');
-//   console.log('User entered: ' + userName + ' for name.');
-//   if (userName) {break;}
-//   alert('Please enter a name!');
-// } while (userName === null);
-// alert('Hello ' + userName + '! Let\'s play a guessing game about me. We\'ll start with 5 yes/no questions about me!');
-
-
-// // Keeps track of all correct responses
-// let correctAnswers = 0;
-
-
-// // 5 yes no questionaire; FYI: all answers are yes btw
-// console.log('*** Starting 5 yes/no questions game');
-// let questionBank = [
-//   'Do you think I like the color black?',
-//   'Could I possibly like to eat sushi?',
-//   'Was I in the Air Force?',
-//   'Do I like to watch cooking shows?',
-//   'Is sleeping my favorite activity?'
-// ];
-// let possibleAns = [
-//   'y',
-//   'n',
-//   'yes',
-//   'no'
-// ];
-
-// for (let i = 0; i < questionBank.length; i++) {
-//   let answer = prompt(questionBank[i] + ' (y/n/yes/no):');
-//   console.log('Question: ' + questionBank[i]);
-//   console.log('User response: ' + answer);
-//   let idx = -1;
-//   if (answer) { idx = possibleAns.indexOf(answer.toLowerCase()); }
-//   if (answer && (idx + 1)) {
-//     if (idx % 2) {
-//       alert('Wrong!');
-//     } else {
-//       alert('Right!');
-//       correctAnswers++;
-//     }
-//   } else {
-//     alert('Not an acceptable response!');
-//     console.log('Not acceptable. Repeating the same question.');
-//     i--;
-//   }
-// }
-
-// // Display answers to 5 questions and correct answers on the page
-// let listItemsQuestions = '';
-
-// for (let i = 0; i < questionBank.length; i++) {
-//   listItemsQuestions += '<li>' + questionBank[i] + ' YES!</li>\n';
-// }
-// document.getElementById('fivequestionlist').innerHTML = listItemsQuestions;
-// alert('You got ' + correctAnswers + ' out of 5 correct so far!');
-
-
-// //Favorite number game
-// console.log('*** Starting guess number game');
-// let maxNumber = 15;
-// let maxGuesses = 4;
-// let guessWord = ' guesses';
-// let response = null;
-// let randomNumber = Math.floor(Math.random() * Math.floor(maxNumber + 1));
-// alert('Let\'s play guess my number game!');
-
-// while (parseInt(response) !== randomNumber && (maxGuesses > 0)) {
-//   response = prompt('Guess my number between 0 and ' + maxNumber + ' (' + maxGuesses + guessWord + ' remaining)');
-//   console.log('User guessed: ' + response);
-//   if (response && !isNaN(response)) {
-//     if (parseInt(response) > randomNumber) {
-//       alert('Mmmm... the number is lower');
-//     }
-//     if (parseInt(response) < randomNumber) {
-//       alert('Actually, the number is higher');
-//     }
-//     maxGuesses--;
-//     if (maxGuesses === 1) {
-//       guessWord = ' guess';
-//     }
-//   } else {
-//     alert('Please enter a number!');
-//   }
-// }
-
 'use strict';
 // Keeps track of all correct responses
 let correctAnswers = 0;
 
 // Get User Name and give intro to guessing game
+// Driver code
 let userName = getUserName();
 alert('Hello ' + userName + '! Let\'s play a guessing game about me. We\'ll start with 5 yes/no questions about me!');
 correctAnswers = gameOne(correctAnswers);
 alert('You got ' + correctAnswers + ' out of 5 correct so far!');
 correctAnswers = gameTwo(correctAnswers);
 alert('You got ' + correctAnswers + ' out of 6 correct so far!');
-// correctAnswers = gameThree(correctAnswers);
+correctAnswers = gameThree(correctAnswers);
+alert('You got ' + correctAnswers + ' out of 7 correct!');
+if (correctAnswers === 7) {
+  alert('You are amazing!');
+} else {
+  alert('Please try again!');
+}
 
-
+//----
 //Funtion to get username and check for  null and empty string
 function getUserName(){
   let userName = null;
@@ -114,6 +30,7 @@ function getUserName(){
   } while (userName === null || userName === '');
   return userName;
 }
+//-----
 //Function to ask first game questions
 function gameOne(correctAnswers){
   // 5 yes no questionaire; FYI: all answers are yes btw
@@ -162,8 +79,8 @@ function gameOne(correctAnswers){
   return correctAnswers;
 }
 
-
-//  Favorite number game
+//---
+//  Favorite number game this function generates gussing number game.
 function gameTwo(correctAnswers){
   console.log('*** Starting guess number game');
   let maxNumber = 15;
@@ -191,8 +108,6 @@ function gameTwo(correctAnswers){
       alert('Please enter a number!');
     }
   }
-
-
   // Results for the guess the number game
   if (parseInt(response) === randomNumber) {
     alert('You guessed the number!');
@@ -206,45 +121,42 @@ function gameTwo(correctAnswers){
   return correctAnswers;
 }
 
-
 // Mulitple choice places visited question
-console.log('*** Starting multiple choice game');
-let placesVisited = ['Nice', 'Stuggart', 'Taipei'];
-let response = null;
-let maxGuesses = 6;
-let guessWord = ' guesses';
-while (!response && (maxGuesses > 0)) {
-  response = prompt('From the following list, which places have I been to? (write the places separated with a space):\nNice, Tokyo, Stuggart, Rome, Vancouver, Taipei\n(' + maxGuesses + guessWord + ' remaining):');
-  console.log('User responded with: ' + response);
-  if (response) {
-    response = response.split(' ');
-    for (let i = 0; i < response.length; i++) {
-      if (!placesVisited.includes(response[i])) {
-        alert('Not quite right...');
-        response = null;
-        maxGuesses--;
-        if (maxGuesses === 1) {
-          guessWord = ' guess';
+function gameThree(correctAnswers){
+  console.log('*** Starting multiple choice game');
+  let placesVisited = ['Nice', 'Stuggart', 'Taipei'];
+  let response = null;
+  let maxGuesses = 6;
+  let guessWord = ' guesses';
+  while (!response && (maxGuesses > 0)) {
+    response = prompt('From the following list, which places have I been to? (write the places separated with a space):\nNice, Tokyo, Stuggart, Rome, Vancouver, Taipei\n(' + maxGuesses + guessWord + ' remaining):');
+    console.log('User responded with: ' + response);
+    if (response) {
+      response = response.split(' ');
+      for (let i = 0; i < response.length; i++) {
+        if (!placesVisited.includes(response[i])) {
+          alert('Not quite right...');
+          response = null;
+          maxGuesses--;
+          if (maxGuesses === 1) {
+            guessWord = ' guess';
+          }
+          break;
         }
-        break;
       }
+    } else {
+      alert('Please enter a response!');
     }
-  } else {
-    alert('Please enter a response!');
   }
-}
-if (response) {
-  alert('Congrats! You figured out where I\'ve been!');
-  correctAnswers++;
-} else {
-  alert('You didn\'t figure it out! I\'ve been to Nice, Stuggart, and Taipei.');
+  if (response) {
+    alert('Congrats! You figured out where I\'ve been!');
+    correctAnswers++;
+  } else {
+    alert('You didn\'t figure it out! I\'ve been to Nice, Stuggart, and Taipei.');
+  }
+
+  // Displays results of multiple choice
+  document.getElementById('multiplechoicelist').innerHTML = '<li>I\'ve been to Nice, Stuggart, and Taipei</li>';
+  return correctAnswers;
 }
 
-// Displays results of multiple choice
-document.getElementById('multiplechoicelist').innerHTML = '<li>I\'ve been to Nice, Stuggart, and Taipei</li>';
-alert('You got ' + correctAnswers + ' out of 7 correct!');
-if (correctAnswers === 7) {
-  alert('You are amazing!');
-} else {
-  alert('Please try again!');
-}
